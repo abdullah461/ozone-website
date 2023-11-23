84eb4af39faf159767148482bcccff84a5679ce3
abdull461 <abdullahabubakar461@gmail.com>
2023-11-23 21:07:33 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// // hamburger menu
+// const hamburger = document.querySelector(".hamburger");
+// const nav_ul = document.querySelector(".links ul");
+// const links = document.querySelectorAll("li");
+
+// hamburger.addEventListener('click', ()=>{
+//  //Animate Links
+//  nav_ul.style.display = "block"
+
+//  nav_ul.classList.toggle("open");
+//   links.forEach(link => {
+//       link.classList.toggle("fade");
+//   });
+
+//   //Hamburger Animation
+//   hamburger.classList.toggle("toggle");
+// });
